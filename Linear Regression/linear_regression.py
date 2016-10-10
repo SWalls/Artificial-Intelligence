3d4ab86a633c8cc0ed70d579da52500b93aa5d20
SWalls <soerenwalls@gmail.com>
2016-10-09 22:15:26 -0600

    Have linear regression almost working.

--- Linear Regression/linear_regression.py
+++ Linear Regression/linear_regression.py
@@ -1,57 +1,41 @@
 import matplotlib.pyplot as plt
 import numpy as np
-np.random.seed(42) # Get the same random numbers every time
+import math
+np.random.seed(422) # Get the same random numbers every time
 
 class LinearRegression:
-    weight = np.random.rand(1)
-    bias = np.random.rand(1)
-    variables = []
-    data = []
 
-    def __init__(self, csvFile, variableCols):
+    def __init__(self, csv_file, variable_cols, learning_rate):
         ### Load the dataset
-        self.data = np.genfromtxt('dija_temp.csv', delimiter=',', skip_header=1)[:10]
-        for col in variableCols:
-            variables.append(data[:, col])
-        
+        self.variables = []
+        self.data = np.genfromtxt(csv_file, delimiter=',', skip_header=1)[:10]
+        for col in variable_cols:
+            self.variables.append(self.data[:, col])
+        ### How do we change the weight and the bias to make the line's fit better?
+        self.learning_rate = learning_rate
 
+    def learn(self):
+        y = self.variables[0] # djia
+        x = self.variables[1] # temperature
+        ### Init the model parameters
+        weight = np.random.randint(math.ceil(y[0]/x[0]), size=1)[0]
+        bias = np.random.randint(math.ceil(y[0]-x[0]), size=1)[0]
+        print "Initial weight: %f, Initial bias: %f" % (weight, bias)
+        cost = 2
+        while cost > 1:
+            error = (x*weight+bias) - y
+            # print error
+            cost = np.sum(np.power(error, 2))
+            # print cost
+            weight -= np.sum(self.learning_rate * error * x / len(x))
+            bias -= np.sum(self.learning_rate * error * 1.0 / len(x))
+            # print "New weight: %f, New bias: %f" % (weight, bias)
+        print "FINAL weight: %f, FINAL bias: %f" % (weight, bias)
+        ### Graph the dataset along with the line defined by the model
+        xs = np.arange(0, 5)
+        ys = xs * weight + bias
+        plt.plot(x, y, 'r+', xs, ys, 'g-')
+        plt.show()
 
-################################################################
-### Init the model parameters
-weight = np.random.rand(1)
-bias = np.random.rand(1)
-print weight, bias
-
-
-################################################################
-### How do we change the weight and the bias to make the line's fit better?
-learning_rate = 0.05
-
-init_cost = np.sum(np.power((hs_gpa*weight+bias) - col_gpa, 2))
-
-error = (hs_gpa*weight+bias) - col_gpa
-
-weight = weight - np.sum(learning_rate * error * hs_gpa / len(hs_gpa))
-bias = bias - np.sum(learning_rate * error * 1.0 / len(hs_gpa))
-
-end_cost = np.sum(np.power((hs_gpa*weight+bias) - col_gpa, 2))
-
-error = (hs_gpa*weight+bias) - col_gpa
-
-weight = weight - np.sum(learning_rate * error * hs_gpa / len(hs_gpa))
-bias = bias - np.sum(learning_rate * error * 1.0 / len(hs_gpa))
-
-end_end_cost = np.sum(np.power((hs_gpa*weight+bias) - col_gpa, 2))
-
-print init_cost
-print end_cost
-print end_end_cost
-
-################################################################
-### Graph the dataset along with the line defined by the model
-#
-# xs = np.arange(0, 5)
-# ys = xs * weight + bias
-#
-# plt.plot(hs_gpa, col_gpa, 'r+', xs, ys, 'g-')
-# plt.show()
+model = LinearRegression(csv_file="djia_temp.csv", variable_cols=[1,2], learning_rate=0.1)
+model.learn()
---
+++ Linear Regression/mw_linear_regression.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+import numpy as np
+np.random.seed(42) # Get the same random numbers every time
+
+
+# Ugly code for thinking about linear regression with gradient descent
+
+################################################################
+### Load the dataset
+my_data = np.genfromtxt('gpa.csv', delimiter=',', skip_header=1)[:10]
+hs_gpa = my_data[:, 0]
+col_gpa = my_data[:, 1]
+
+
+################################################################
+### Init the model parameters
+weight = np.random.rand(1)
+bias = np.random.rand(1)
+print weight, bias
+
+
+################################################################
+### How do we change the weight and the bias to make the line's fit better?
+learning_rate = 0.05
+
+error = (hs_gpa*weight+bias) - col_gpa
+
+init_cost = np.sum(np.power(error, 2))
+
+weight = weight - np.sum(learning_rate * error * hs_gpa / len(hs_gpa))
+bias = bias - np.sum(learning_rate * error * 1.0 / len(hs_gpa))
+
+print weight, bias
+
+error = (hs_gpa*weight+bias) - col_gpa
+
+end_cost = np.sum(np.power(error, 2))
+
+weight = weight - np.sum(learning_rate * error * hs_gpa / len(hs_gpa))
+bias = bias - np.sum(learning_rate * error * 1.0 / len(hs_gpa))
+
+print weight, bias
+
+error = (hs_gpa*weight+bias) - col_gpa
+
+end_end_cost = np.sum(np.power(error, 2))
+
+print init_cost
+print end_cost
+print end_end_cost
+
+################################################################
+### Graph the dataset along with the line defined by the model
+#
+# xs = np.arange(0, 5)
+# ys = xs * weight + bias
+#
+# plt.plot(hs_gpa, col_gpa, 'r+', xs, ys, 'g-')
+# plt.show()
