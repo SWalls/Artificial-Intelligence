0c3f38ece6b2a677ac0d3eb096ad97a4647931cc
SWalls <soerenwalls@gmail.com>
2016-10-13 00:11:51 -0600

    Updated neural network code so it works mostly. Something is still wrong with forward or back propogation.

--- Neural Network/neural_network.py
+++ Neural Network/neural_network.py
@@ -1,39 +1,88 @@
 import matplotlib.pyplot as plt
 import numpy as np
 np.random.seed(42)
+        
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def dsigmoid(x):
+    return x * (1 - x)
 
 class NetworkLayer(object):
 
+    def forward(self, X):
+        return X
+
+    def backward(self, err):
+        return err
+
+class DenseNetworkLayer(NetworkLayer):
+
     def __init__(self, size, next_layer_size, learning_rate):
-        self.size = size
         self.weights = np.random.rand(size, next_layer_size)
         self.learning_rate = learning_rate
-
-    def thresholdFunction(self, x):
-        return x
-
-    def thresholdFunctionDeriv(self, x):
-        return x
 
     def forward(self, X):
         self.incoming = X
         act = X.dot(self.weights)
-        act = self.thresholdFunction(act)
+        return act
+
+    def backward(self, err):
+        update = self.incoming.T.dot(err)
+        corrected = err.dot(self.weights.T)
+        self.weights += self.learning_rate * update
+        return corrected
+
+class SigmoidNetworkLayer(NetworkLayer):
+
+    def forward(self, X):
+        act = sigmoid(X)
         self.outputs = act
         return act
 
     def backward(self, err):
-        err = err * self.thresholdFunctionDeriv(self.outputs)
-        update = self.incoming.T.dot(err)
-        self.weights += self.learning_rate * update
-        return update
+        err = (err * dsigmoid(self.outputs))
+        return err
 
-    def reportAccuracy(self, X, y):
-        out = self.forward(X)
-        out = np.round(out)
-        count = np.count_nonzero(y - out)
-        correct = len(X) - count
-        print "%.4f" % (float(correct)*100.0 / len(X))
+class NeuralNetwork:
+
+    def __init__(self):
+        self.layers = []
+    
+    def addLayer(self, layer):
+        self.layers.append(layer)
+
+    def propogateForward(self, X):
+        # propogate forward
+        incoming = X
+        for i in range(0, len(self.layers)):
+            incoming = self.layers[i].forward(incoming)
+        # calculate error (now, incoming = final output prediction)
+        # err = self.layers[-1].calculateError(y, incoming)
+        return incoming
+
+    def propogateBackward(self, err):
+        # propogate backward
+        # iterate in reverse order
+        outgoing = err
+        for i in range(len(self.layers)-1, -1, -1):
+            outgoing = self.layers[i].backward(outgoing)
+        # calculate error (now, incoming = final output prediction)
+        # err = self.layers[-1].calculateError(y, incoming)
+        return outgoing
+
+    def iteration(self, i, X, y):
+        pred = self.propogateForward(X)
+        deriv_err = self.calculateDerivError(y, pred)
+        change = self.propogateBackward(deriv_err)
+        self.reportAccuracy(i, y, pred)
+        return change
+
+    def reportAccuracy(self, i, y, pred):
+        pred = np.round(pred)
+        count = np.count_nonzero(y - pred)
+        correct = len(pred) - count
+        print "Epoch %d: %.4f%% correct" % (i, float(correct)*100.0 / len(pred))
 
     def calculateDerivError(self, y, pred):
         return 2*(y - pred)
@@ -41,68 +90,55 @@ class NetworkLayer(object):
     def calculateError(self, y, pred):
         return (np.sum(np.power((y - pred), 2)))
 
-class SigmoidNetworkLayer(NetworkLayer):
-
-    def thresholdFunction(self, x):
-        return 1 / (1 + np.exp(-x))
-
-    def thresholdFunctionDeriv(self, x):
-        return x * (1 - x)
-
-class NeuralNetwork:
-
-    def __init__(self, layer_sizes, learning_rate):
-        self.layers = []
-        nextLayerSize = 1
-        for i in range(len(layer_sizes)-1, -1, -1):
-            self.layers.insert(0, SigmoidNetworkLayer(layer_sizes[i], nextLayerSize, learning_rate))
-            nextLayerSize = layer_sizes[i]
-
-    def iteration(self, X, y):
-        incoming = X
-        for layer in self.layers:
-            out = layer.forward(incoming)
-            incoming = out
-        err = self.layers[-1].calculateError(y, incoming)
-        print err
-        deriv_err = self.layers[-1].calculateDerivError(y, incoming)
-        for layer in self.layers:
-            layer.backward(deriv_err)
-
     def train(self, X, y, epochs):
         for i in range(0, epochs):
-            self.iteration(X, y)
+            self.iteration(i, X, y)
 
-    def gradientChecker(model, X, y):
+    def checkGradient(self, X, y):
         epsilon = 1E-5
+        wShape = self.layers[0].weights.shape
+        twoDim = True if len(wShape) > 1 else False
 
-        model.weights[1] += epsilon
-        out1 = model.forward(X)
-        err1 = model.calculateError(y, out1)
+        ### Adjust weight slightly to the right
+        if twoDim: self.layers[0].weights[1][0] += epsilon
+        else: self.layers[0].weights[1] += epsilon
+        pred1 = self.propogateForward(X)
+        err1 = self.calculateError(y, pred1)
 
-        model.weights[1] -= 2*epsilon
-        out2 = model.forward(X)
-        err2 = model.calculateError(y, out2)
+        ### Adjust weight slightly to the left
+        if twoDim: self.layers[0].weights[1][0] -= 2*epsilon
+        else: self.layers[0].weights[1] -= 2*epsilon
+        pred2 = self.propogateForward(X)
+        err2 = self.calculateError(y, pred2)
 
+        ### Calculate midpoint of errors
+        print err1, err2
         numeric = (err2 - err1) / (2*epsilon)
-        print numeric
+        print "Error midpoint: %f" % numeric
 
-        model.weights[1] += epsilon
-        out3 = model.forward(X)
-        err3 = model.calculateDerivError(y, out3)
-        derivs = model.backward(err3)
-        print derivs[1]
+        ### Calculate derivative of the real error to see if it matches
+        # adjust weight back to normal
+        if twoDim: self.layers[0].weights[1][0] += epsilon
+        else: self.layers[0].weights[1] += epsilon
+        change = self.iteration(0,X,y)
 
-def loadDataset(filename='breast_cancer.csv'):
-    my_data = np.genfromtxt(filename, delimiter=',', skip_header=1)
+        if twoDim: print "Real derivative: %f" % change[1][0]
+        else: print "Real derivative: %f" % change[1]
+
+def loadDataset(filename='breast_cancer.csv', rows=0):
+    if(rows == 0):
+        my_data = np.genfromtxt(filename, delimiter=',', skip_header=1)
+    else:
+        my_data = np.genfromtxt(filename, delimiter=',', skip_header=1)[:rows]
 
     # The labels of the cases
     # Raw labels are either 4 (cancer) or 2 (no cancer)
     # Normalize these classes to 0/1
     y = (my_data[:, 10] / 2) - 1
+    y = y.reshape((y.shape[0],1))
 
     # Case features
-    X = my_data[:, :10]
+    X = my_data[:, 1:10]
 
     # Normalize the features to (0, 1)
     X_norm = X / X.max(axis=0)
@@ -110,11 +146,17 @@ def loadDataset(filename='breast_cancer.csv'):
     return X_norm, y
 
 if __name__=="__main__":
-    X, y = loadDataset()
-    # X = X
+    np.set_printoptions(precision=2)
+    X, y = loadDataset('breast_cancer.csv', 50)
+    print "\nX-norm:"
     print X
+    print "\nY:"
     print y
-    print X.shape, y.shape
-    model = NeuralNetwork([10], 0.05)
-    # gradientChecker(model, X, y)
-    model.train(X, y, 100)
+    print "\nX-shape: %s, Y-shape: %s" % (X.shape, y.shape)
+    model = NeuralNetwork()
+    model.addLayer(DenseNetworkLayer(9,9,0.01))
+    model.addLayer(SigmoidNetworkLayer())
+    model.addLayer(DenseNetworkLayer(9,1,0.01))
+    model.addLayer(SigmoidNetworkLayer())
+    model.checkGradient(X, y)
+    model.train(X, y, 10)
